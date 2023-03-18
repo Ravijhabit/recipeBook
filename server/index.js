98032d42659f16bf197e78cf6989ee4e9605cb1f
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2023-03-18 13:08:47 +0530

    added login and register route

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { userRouter } from './routes/users.js'; 
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use('/auth', userRouter);
+
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console,'Error Connection'));
+db.once('open', function(){
+    console.log('Connected successfully')
+});
+
+app.listen(3001, ()=>{
+    console.log('Server Started');
+});
